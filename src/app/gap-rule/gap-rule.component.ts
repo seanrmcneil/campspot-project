4df1cb37eab8498de87a7a55e0d31e9c41aa3c33
Sean McNeil <seanmcneil@Seans-MBP.home>
2018-08-04 09:33:47 -0400

    saving with date, going to try with moment

--- src/app/gap-rule/gap-rule.component.ts
+++ src/app/gap-rule/gap-rule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-gap-rule',
@@ -9,28 +10,68 @@ export class GapRuleComponent implements OnInit {
   campInfo: string;
   JSONCampInfo: JSON;
   fileToUpload: File = null;
+  availableCampsites: any;
+  campsiteReservations = {}; //make a typescrpipt object later
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.campInfo);
-  }
-
-  onSearch() {
-    this.JSONCampInfo = JSON.parse(this.campInfo)
-    console.log(JSON.parse(this.campInfo));
-    console.log(this.JSONCampInfo.reservations);
-
   }
 
   uploadForm(files: FileList){
     this.fileToUpload = files.item(0);
-
     let fileReader = new FileReader();
     fileReader.onload = (e) => {
       this.campInfo = (fileReader.result)
     };
     fileReader.readAsText(this.fileToUpload);
+  }
+
+  onSearch() {
+    this.JSONCampInfo = JSON.parse(this.campInfo)
+    console.log(JSON.parse(this.campInfo));
+    console.log(this.JSONCampInfo['reservations']);
+    this.calcCampsiteCalendar();
+    this.checkAvailability()
+  }
+
+  calcCampsiteCalendar(){
+    let reservations = this.JSONCampInfo['reservations'];
+    reservations.forEach((item) => {
+         let reservation = [moment(item['startDate']), moment(item['endDate']+'Z')];
+         console.log(reservation);
+         if (item.campsiteId in this.campsiteReservations){
+           this.campsiteReservations[item['campsiteId']].push(reservation)
+         }else {
+           this.campsiteReservations[item['campsiteId']] = [reservation]
+         }
+    });
+  }
+
+  checkAvailability(){
+    let campsites = this.JSONCampInfo['campsites'];
+    let searchStartDate = new Date(this.JSONCampInfo['search']['startDate']);
+    let searchEndDate = new Date(this.JSONCampInfo['search']['endDate']);
+    this.availableCampsites = [];
+    campsites.forEach((item) =>{
+      if(item.id in this.campsiteReservations){
+        console.log(this.campsiteReservations[item.id]);
+        this.campsiteReservations[item.id].forEach((reservation) => {
+          console.log(reservation);
+          // var tomorrow = new Date();
+          // tomorrow.setDate(tomorrow.getDate() + 1);
+
+          console.log(reservation[1].setDate(reservation[1] + 1));
+          if(searchStartDate == reservation[1].getDate() +1){
+            console.log("ONE DAY GAP");
+          }
+          // return (searchEndDate >= startdate && startD <= enddate);
+        });
+      }else{
+        //the campsite has no reservations so you can book
+        this.availableCampsites.push(item.name)
+      }
+    })
   }
 
 }
